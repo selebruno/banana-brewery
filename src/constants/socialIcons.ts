0c43888fc33269de4151cd71e6874f93a672b43d
Selene Bruno <78818455+selebruno@users.noreply.github.com>
2024-02-29 22:58:49 +0100

    Add files via upload

---
+++ src/constants/socialIcons.ts
@@ -0,0 +1,22 @@
+export const socialIcons = [
+  {
+    name: "instagram",
+    url: "https://instagram.com",
+  },
+  {
+    name: "tiktok",
+    url: "https://tiktok.com",
+  },
+  {
+    name: "x",
+    url: "https://x.com",
+  },
+  {
+    name: "youtube",
+    url: "https://youtube.com",
+  },
+  {
+    name: "twitch",
+    url: "https://twitch.com",
+  },
+];
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { SocialIcon } from "react-social-icons";
+import BananaLogo from "assets/banana.png";
+import { socialIcons } from "constants/socialIcons";
+
+const Container = styled.div`
+  padding: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 10rem;
+    height: 14rem;
+  }
+`;
+
+const SocialIconsContainer = styled.span`
+  display: flex;
+  flex-direction: column;
+  padding-top: 2rem;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const SocialIconWrapper = styled.div`
+  margin: 8px;
+`;
+
+export default function Home() {
+  const [hovering, setHovering] = useState<number | null>(null);
+  return (
+    <Container className="App">
+      <img src={BananaLogo} alt="Banana Logo" />
+      <SocialIconsContainer>
+        {socialIcons.map((icon, index) => (
+          <SocialIconWrapper key={index}>
+            <SocialIcon
+              bgColor={
+                hovering === index ? "var(--secondary-color)" : "transparent"
+              }
+              onMouseEnter={() => setHovering(index)}
+              onMouseLeave={() => setHovering(null)}
+              fgColor={
+                hovering === index
+                  ? "var(--primary-color)"
+                  : "var(--secondary-color)"
+              }
+              url={icon.url}
+              network={icon.name}
+              style={{ width: "70px", height: "70px" }} // Default size, adjust as needed
+            />
+          </SocialIconWrapper>
+        ))}
+      </SocialIconsContainer>
+    </Container>
+  );
+}
